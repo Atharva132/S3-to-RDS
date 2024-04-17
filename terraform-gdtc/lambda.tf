ed11b0f2399089af54973df7d97329ce2a27a38b
Atharva132 <atharvaamte@gmail.com>
2024-04-17 19:47:36 +0530

    removed output

--- terraform-gdtc/lambda.tf
+++ terraform-gdtc/lambda.tf
@@ -104,7 +104,3 @@ resource "aws_iam_role_policy_attachment" "lambda_execution_policy_attachment" {
   role       = aws_iam_role.s3_to_rds_function_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-output "endpoint" {
-  value = local.rds_endpoint_without_port
-}
